37de72432df79ba20bfa48ba14019e8df0331b59
zenyhoj <joe.balingit@gmail.com>
2022-04-06 08:08:11 +0800

    mysql backup set up and running

---
+++ mysql_auto_backup.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 3/11/2022
+ * Time: 3:14 PM
+ */
+
+$databases = ['billing'];
+$user = "waterdistrict";
+$pass = "buenawaters";
+$host = "192.168.1.9";
+date_default_timezone_set("Asia/Singapore");
+
+if(!file_exists("D:/PublicFiles/db_backups/billingDB")){
+    mkdir("D:/PublicFiles/db_backups/billingDB");
+}
+
+foreach($databases as $database) {
+    if(!file_exists("D:/PublicFiles/db_backups/billingDB/$database"))
+    {
+        mkdir("D:/PublicFiles/db_backups/billingDB/$database");
+
+    }
+
+
+    $filename = $database."_".date("F_d_Y")."@".date("g_ia").uniqid("_", false);
+    $folder = "D:/PublicFiles/db_backups/billingDB/$database/".$filename.".sql";
+
+    exec("C:/xampp/mysql/bin/mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$folder}", $output);
+}
+
+//print_r($output);
